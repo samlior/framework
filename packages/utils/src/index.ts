24ac187d7f1276730307f658375bd48dd114f741
samlior <samlior@foxmail.com>
2023-01-19 11:56:16 +0800

    chore: improve code style

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./channel";
 export * from "./counter";
 export * from "./events";
-export * from "./helper";
 export * from "./jsonrpc";
 export * from "./limited";
 export * from "./log";
